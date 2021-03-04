4c5e0534a41fc0961fa1c4eec86a2175a8798eac
pazarbasifatih <42507858+pazarbasifatih@users.noreply.github.com>
2021-03-04 03:26:41 +0300

    Update entrypoint.sh

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-["java", "-jar" ,"/tika-server-${TIKA_VERSION}.jar", "-h", "0.0.0.0", "-p", "9998", "-enableUnsecureFeatures", "-enableFileUrl", "--config", "${GENERATE_FILE}", "--cors", "i18n-48e8c.appspot.com/*"]  
+["bin/bash", "java", "-jar" ,"/tika-server-${TIKA_VERSION}.jar", "-h", "0.0.0.0", "-p", "9998", "-enableUnsecureFeatures", "-enableFileUrl", "--config", "${GENERATE_FILE}", "--cors", "i18n-48e8c.appspot.com/*"]  
